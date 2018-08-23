285aa17e267c7b7b7f88028c387ac4116ffe6c67
Yurii Nosach <nosach.yurko@gmail.com>
2018-08-23 11:57:14 +0300

    Added "USP_Color_Create" and fixed "fn_Color_Exists"

---
+++ TicketMS-Database/TicketMS-Database/dbo/Stored Procedures/USP_Color_Create.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[USP_Color_Create]
+	@name			NVARCHAR(32),
+	@paletteName	NVARCHAR(32)
+AS
+	IF ([dbo].[fn_Color_Exists](@name, @paletteName) = 1)
+	BEGIN;
+		DECLARE @msg NVARCHAR(MAX) = CONCAT(N'Колір з назвою "', @name, N'" або кодом "', @paletteName, N'" вже існує.');
+		THROW 50001, @msg, 1;
+	END
+
+	INSERT INTO [Color]([Name], [PaletteName])
+		VALUES (@name, @paletteName)
+RETURN 0
