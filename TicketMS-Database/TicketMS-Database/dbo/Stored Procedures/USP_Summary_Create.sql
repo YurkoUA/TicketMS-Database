0a14f6d5dd296ed697d026717f4522df75f5ac2c
Yurii Nosach <nosach.yurko@gmail.com>
2018-08-25 14:12:55 +0300

    USP_Summary_Create: added checking if summary exists.

--- TicketMS-Database/TicketMS-Database/dbo/Stored Procedures/USP_Summary_Create.sql
+++ TicketMS-Database/TicketMS-Database/dbo/Stored Procedures/USP_Summary_Create.sql
@@ -1,9 +1,16 @@
 ﻿CREATE PROCEDURE [dbo].[USP_Summary_Create]
 AS
+	DECLARE @date DATETIME = GETUTCDATE()
+
+	IF EXISTS (SELECT 1 FROM [Summary] WHERE YEAR([Date]) = YEAR(@date) AND MONTH([Date]) = MONTH(@date))
+	BEGIN;
+		THROW 50001, N'Підсумок за даний місяць вже існує.', 1;
+	END
+
 	DECLARE @packagesCount INT = (SELECT COUNT(*) FROM [Package])
 
 	INSERT INTO [Summary] ([Date], [Tickets], [HappyTickets], [Packages])
-	SELECT	GETUTCDATE(),
+	SELECT	@date,
 			COUNT(*),
 			SUM(CASE [dbo].[fn_Number_IsHappy]([Number]) WHEN 1 THEN 1 ELSE 0 END),
 			@packagesCount
