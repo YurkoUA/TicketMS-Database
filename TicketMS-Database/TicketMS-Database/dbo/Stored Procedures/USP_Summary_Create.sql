e95969a11f7dba70b2197f8c29e15794012027ab
Yurii Nosach <nosach.yurko@gmail.com>
2018-08-25 14:08:36 +0300

    Added "USP_Summary_Create"

---
+++ TicketMS-Database/TicketMS-Database/dbo/Stored Procedures/USP_Summary_Create.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[USP_Summary_Create]
+AS
+	DECLARE @packagesCount INT = (SELECT COUNT(*) FROM [Package])
+
+	INSERT INTO [Summary] ([Date], [Tickets], [HappyTickets], [Packages])
+	SELECT	GETUTCDATE(),
+			COUNT(*),
+			SUM(CASE [dbo].[fn_Number_IsHappy]([Number]) WHEN 1 THEN 1 ELSE 0 END),
+			@packagesCount
+
+			FROM [Ticket]
+RETURN 0
