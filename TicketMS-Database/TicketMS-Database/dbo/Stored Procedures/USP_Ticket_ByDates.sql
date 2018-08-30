f2116e432c952aee689db56f93182c659e375047
Yurii Nosach <nosach.yurko@gmail.com>
2018-08-30 08:53:39 +0300

    Added SP which gets count of tickets by dates.

---
+++ TicketMS-Database/TicketMS-Database/dbo/Stored Procedures/USP_Ticket_ByDates.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[USP_Ticket_ByDates]
+	@startDate		DATETIME,
+	@endDate		DATETIME,
+	@hoursOffset	INT = 0
+AS
+	SELECT	CAST(DATEADD(HOUR, @hoursOffset, [CreatedDate]) AS DATE)	AS [Date],
+		COUNT(*)														AS [TicketsCount]
+
+	FROM [Ticket]
+	WHERE [CreatedDate] BETWEEN @startDate AND @endDate
+
+	GROUP BY CAST(DATEADD(HOUR, @hoursOffset, [CreatedDate]) AS DATE)
+	ORDER BY [Date] ASC
+RETURN 0
