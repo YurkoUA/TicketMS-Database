46c402cd8c6f0769b9f30b7308f54be8dd85bf3c
Yurii Nosach <nosach.yurko@gmail.com>
2018-09-27 13:40:01 +0300

    Improved USP_Package_Filter (onlyOpened, hideSpecial).

--- TicketMS-Database/TicketMS-Database/dbo/Stored Procedures/USP_Package_Filter.sql
+++ TicketMS-Database/TicketMS-Database/dbo/Stored Procedures/USP_Package_Filter.sql
@@ -2,7 +2,10 @@
 	@firstDigit INT = NULL,
 	@serialId	INT = NULL,
 	@colorId	INT = NULL,
-	@nominalId	INT = NULL
+	@nominalId	INT = NULL,
+
+	@onlyOpened		BIT = 0,
+	@hideSpecial	BIT = 1
 AS
 	SELECT *
 	FROM [v_Packages] AS [p]
@@ -10,6 +13,8 @@ AS
 		AND		((@serialId IS NOT NULL		AND [p].[SerialId] = @serialId)				OR @serialId IS NULL)
 		AND		((@colorId IS NOT NULL		AND [p].[ColorId] = @colorId)				OR @colorId IS NULL)
 		AND		((@nominalId IS NOT NULL	AND [p].[NominalId] = @nominalId)			OR @nominalId IS NULL)
+		AND		((@onlyOpened = 1			AND [p].[IsOpened] = 1)						OR @onlyOpened = 0)
+		AND		((@hideSpecial = 1			AND [p].[IsSpecial] = 0)					OR @hideSpecial = 0)
 
 ORDER BY [p].[PackageId]
 RETURN 0
